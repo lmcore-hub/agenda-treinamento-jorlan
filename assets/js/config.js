9178d33eb5707be14307b9cbe67d13b435824be7
lmcore-hub <lctmarques1502@gmail.com>
2026-06-22 11:17:25 -0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-window.APP_CONFIG = {
+window.JORLAN_TRAINING_CONFIG = {
   SUPABASE_URL: "https://knpcfkexhqpihpfrvpzg.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_o3lCS5C25XEw-cAjAprrxA_3KLBn_ws"
 };
